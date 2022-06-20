cad970e58f74fb3f436a0e8a17b8f5b8f00f201a
teymie <65744646+teymie@users.noreply.github.com>
2022-06-20 15:48:26 -0700

    my blog app

---
+++ I4G003194RDE/blog/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.views.generic import CreateView
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from .models import Post
+
+# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(Createiew):
+    model = Post
+
+fields = “__all__”
+
+success_url  = reverse_lazy(“blog:all”)
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+
+fields = “__all__”
+
+success_url  = reverse_lazy(“blog:all”)
+
+class PostDeleteView(DeleteView):
+    model = Post
+
+fields = “__all__”
+
+success_url  = reverse_lazy(“blog:all”)
