8e8e500f9ed99bf8bd3ce4155f4578daacf43e03
teymie <65744646+teymie@users.noreply.github.com>
2022-06-20 17:40:19 -0700

    made few changes

--- I4G003194RDE/blog/views.py
+++ I4G003194RDE/blog/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView
 from django.views.generic import DetailView
 from django.views.generic import UpdateView
 from django.views.generic import DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 # Create your views here.
@@ -11,12 +12,12 @@ class PostListView(ListView):
     model = Post
 
 
-class PostCreateView(Createiew):
+class PostCreateView(CreateView):
     model = Post
 
-fields = “__all__”
+    fields = "__all__"
 
-success_url  = reverse_lazy(“blog:all”)
+    success_url  = reverse_lazy("blog:all")
 
 class PostDetailView(DetailView):
     model = Post
@@ -24,13 +25,13 @@ class PostDetailView(DetailView):
 class PostUpdateView(UpdateView):
     model = Post
 
-fields = “__all__”
+    fields = "__all__"
 
-success_url  = reverse_lazy(“blog:all”)
+    success_url  = reverse_lazy("blog:all")
 
 class PostDeleteView(DeleteView):
     model = Post
 
-fields = “__all__”
+    fields = "__all__"
 
-success_url  = reverse_lazy(“blog:all”)
+    success_url  = reverse_lazy("blog:all")
